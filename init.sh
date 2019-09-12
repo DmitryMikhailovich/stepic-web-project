fb300cb7131543657cccf599c044603e5e026467
Dmitry <dmitry.mm@icloud.com>
2019-09-12 23:51:10 +0300

    some fixes

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-sudo ln -s /home/$(whoami)/web/etc/nginx.conf /etc/nginx/conf.d/stepic.conf
+cp -r -f ./web /home/box
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo nginx -s reload
