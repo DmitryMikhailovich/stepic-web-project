66221656d9646c06aaf30a077876ff5767e94aea
Dmitry <dmitry.mm@icloud.com>
2019-09-20 23:16:07 +0300

    fix urls

--- web/ask/qa/urls.py
+++ web/ask/qa/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
 	url(r'^question/\d+/?$', views.test),
-        url(r'^$', views.return_404),
-        url(r'^login/?$', views.return_404),
-        url(r'^signup/?$', views.return_404),
-        url(r'^ask/?$', views.return_404),
-        url(r'^popular/?$', views.return_404),
-        url(r'^new/?$', views.return_404),
+        url(r'^$', views.test),
+        url(r'^login/?$', views.test),
+        url(r'^signup/?$', views.test),
+        url(r'^ask/?$', views.test),
+        url(r'^popular/?$', views.test),
+        url(r'^new/?$', views.test),
 ]
