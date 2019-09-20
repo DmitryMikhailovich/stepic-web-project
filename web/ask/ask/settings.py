59bb95993e15cbd3fb06a375420da41e4012c4b0
Dmitry <dmitry.mm@icloud.com>
2019-09-20 22:48:34 +0300

    add allowed hosts, turn off debug

--- web/ask/ask/settings.py
+++ web/ask/ask/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^qlniu8gawhbb$+l=p&5b2usbn8xkgw!o5g25l3rbrij5r%6z3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
