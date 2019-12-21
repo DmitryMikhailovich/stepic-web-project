85bd23ca17fdfbee1053502cb1d65e8e3331125f
dmitry <dmitry.mm@icloud.com>
2019-12-21 20:12:34 +0300

    add call to migrate.py to create qa models

--- init.sh
+++ init.sh
@@ -4,7 +4,11 @@ cp -r -f $DIR/web /home/box
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
+
 source $DIR/create_db.sh
+python $DIR/web/ask/manage.py makemigrations
+python $DIR/web/ask/manage.py migrate
+
 gunicorn --config=/etc/gunicorn.d/hello.py hello:application &
 gunicorn --config=/etc/gunicorn.d/ask.py ask.wsgi:application &
 sudo nginx -s reload
