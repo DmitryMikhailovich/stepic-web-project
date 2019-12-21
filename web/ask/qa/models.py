37a554dd98a81d29fd0d8d5373fea354d4572d96
dmitry <dmitry.mm@icloud.com>
2019-12-21 20:44:22 +0300

    make question.rating nullable

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -14,7 +14,7 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(null=True)
     author = models.ForeignKey(to=contrib_models.User, on_delete=models.DO_NOTHING, related_name='question_author')
     likes = models.ManyToManyField(to=contrib_models.User, related_name='likes')
     objects = QuestionManager()
