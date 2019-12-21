e9f3ed92951c7b296c1eb0d3a213649a2da69d5a
dmitry <dmitry.mm@icloud.com>
2019-12-21 20:17:55 +0300

    fix path to web app

--- init.sh
+++ init.sh
@@ -6,8 +6,8 @@ sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
 
 source $DIR/create_db.sh
-python $DIR/web/ask/manage.py makemigrations
-python $DIR/web/ask/manage.py migrate
+python /home/box/web/ask/manage.py makemigrations
+python /home/box/web/ask/manage.py migrate
 
 gunicorn --config=/etc/gunicorn.d/hello.py hello:application &
 gunicorn --config=/etc/gunicorn.d/ask.py ask.wsgi:application &
