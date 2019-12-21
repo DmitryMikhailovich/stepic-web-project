83b5bd2846229bb255c03518be6c6c50ed7298b4
dmitry <dmitry.mm@icloud.com>
2019-12-21 20:10:24 +0300

    set defaults for timestamp fields

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -13,7 +13,7 @@ class QuestionManager(models.Manager):
 class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField()
     author = models.ForeignKey(to=contrib_models.User, on_delete=models.DO_NOTHING, related_name='question_author')
     likes = models.ManyToManyField(to=contrib_models.User, related_name='likes')
@@ -22,6 +22,6 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(to=Question, on_delete=models.DO_NOTHING, related_name='question_answer')
     author = models.ForeignKey(to=contrib_models.User, on_delete=models.DO_NOTHING, related_name='answer_author')
