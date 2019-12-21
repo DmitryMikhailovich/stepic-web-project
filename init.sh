646ef388431d0e4c60d21adac0d468d67657b522
dmitry <dmitry.mm@icloud.com>
2019-12-21 18:52:07 +0300

    solution for 2.3

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
-cp -r -f ./web /home/box
+DIR=$(dirname "$0")
+mkdir /etc/gunicorn.d
+cp -r -f $DIR/web /home/box
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/ask.py /etc/gunicorn.d/ask.py
-
+source $DIR/create_db.sh
 gunicorn --config=/etc/gunicorn.d/hello.py hello:application &
 gunicorn --config=/etc/gunicorn.d/ask.py ask.wsgi:application &
 sudo nginx -s reload
--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+import django.contrib.auth.models as contrib_models
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        pass
+
+    def popular(self):
+        pass
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.CharField(max_length=255)
+    likes = models.ManyToManyField(to=contrib_models.User)
+    objects = QuestionManager()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(to=Question, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(to=contrib_models.User, on_delete=models.DO_NOTHING)
